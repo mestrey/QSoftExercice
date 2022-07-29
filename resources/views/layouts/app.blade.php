a09ea17a86abd59e71f055a2418e261534fb4a11
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-07-29 23:46:34 +0300

    220588 / Интеграция верстки главной страницы - Самопроверка задачи

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,13 +15,11 @@
 
 <body class="bg-white text-gray-600 font-sans leading-normal text-base tracking-normal flex min-h-screen flex-col">
     <div class="wrapper flex flex-1 flex-col">
-
-        <x-panels.header />
+        @include('layouts.parts.header')
 
         @yield('content')
 
-        <x-panels.footer />
-
+        @include('layouts.parts.footer')
     </div>
 </body>
 
