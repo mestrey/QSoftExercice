0d8af9d6a0febd309d34e025c0fcfeba80774ec0
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-07-30 10:23:02 +0300

    220590 / Новости - Самопроверка задачи

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -81,29 +81,7 @@ Carbon\Carbon::setLocale('ru');
             <span class="inline-block text-gray-200 pl-1"> / <a href="news.html" class="inline-block pl-1 text-gray-200 hover:text-orange"><b>Все</b></a></span>
         </div>
         <div class="grid md:grid-cols-1 lg:grid-cols-3 gap-6">
-            @foreach ($articles as $article)
-            <div class="w-full flex">
-                <div class="h-48 lg:h-auto w-32 sm:w-60 lg:w-32 xl:w-48 flex-none text-center overflow-hidden">
-                    <a class="block w-full h-full hover:opacity-75" href="article.html"><img src="{{ asset('pictures/car_ceed.png') }}" class="bg-white bg-opacity-25 w-full h-full object-contain" alt=""></a>
-                </div>
-                <div class="px-4 flex flex-col justify-between leading-normal">
-                    <div class="mb-8">
-                        <div class="text-white font-bold text-xl mb-2">
-                            <a class="hover:text-orange" href="article.html">{{ $article->title }}</a>
-                        </div>
-                        <p class="text-gray-300 text-base">
-                            <a class="hover:text-orange" href="article.html">{{ $article->description }}</a>
-                        </p>
-                    </div>
-                    <div>
-                        <span class="text-sm text-white italic rounded bg-orange px-2">Киа Seed</span>
-                    </div>
-                    <div class="flex items-center">
-                        <p class="text-sm text-gray-400 italic">{{ Carbon\Carbon::parse($article->published_at)->translatedFormat('d M Y') }}</p>
-                    </div>
-                </div>
-            </div>
-            @endforeach
+            <x-panels.articles :articles="$articles" />
         </div>
     </section>
 </main>
