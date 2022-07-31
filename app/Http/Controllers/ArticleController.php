be54f089e0ee971b7120d649aa324e6bf47eae65
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-07-31 13:31:24 +0300

    220591 / Страница списка - изменение url на /articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -19,7 +19,7 @@ class ArticleController extends Controller
             ->latest('published_at')
             ->get();
 
-        return view('pages.news', [
+        return view('pages.articles', [
             'articles' => $articles
         ]);
     }
