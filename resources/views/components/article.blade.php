b65ac84a7b5f5ce91ec9b92a9b9ceec75295990d
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-01 15:26:32 +0300

    220591 / Страница списка - Исправление date

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -16,7 +16,7 @@
                 <span class="text-sm text-white italic rounded bg-orange px-2">Теги</span>
             </div>
             <div class="flex items-center">
-                <p class="text-sm text-gray-400 italic">{{ Carbon\Carbon::parse($article->published_at)->translatedFormat('d M Y') }}</p>
+                <p class="text-sm text-gray-400 italic">{{ $article->published_at->translatedFormat('d M Y') }}</p>
             </div>
         </div>
     </div>
