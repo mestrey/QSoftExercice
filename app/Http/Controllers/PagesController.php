618d8d9b8df914960f6e692e031cc298efde0a80
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-03 17:45:17 +0300

    220598 / Работа с коллекциями и хелперами

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Car;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Str;
 
 class PagesController extends BaseController
 {
@@ -32,6 +33,39 @@ class PagesController extends BaseController
 
     public function client()
     {
+        $cars = Car::with('carEngine', 'carClass')->get();
+
+        $averagePrice = $cars->avg('price');
+        dump($averagePrice);
+
+        $averagePriceDiscounted = $cars->whereNotNull('old_price')->avg('price');
+        dump($averagePriceDiscounted);
+
+        $mostExpensive = $cars->max('price');
+        dump($mostExpensive);
+
+        $salons = $cars->pluck('salon');
+        dump($salons);
+
+        $engines = $cars->pluck('carEngine.name')->sort();
+        dump($engines);
+
+        $classes = $cars->pluck('carClass.name')->keyBy(function ($value, $key) {
+            return Str::slug($value);
+        })->sort();
+        dump($classes);
+
+        $discounted = $cars->whereNotNull('old_price')->reject(function ($e) {
+            $str = $e->name . $e->carEngine->name . $e->kpp;
+            return !str_contains($str, '5') && !str_contains($str, '6');
+        });
+        dump($discounted);
+
+        $bodies = $cars->whereNull('old_price')->pluck('car_body_id', 'carBody.name')->map(function ($e) use ($cars) {
+            return $cars->where('car_body_id', $e)->avg('price');
+        })->sort();
+        dump($bodies);
+
         return view('pages.client');
     }
 
