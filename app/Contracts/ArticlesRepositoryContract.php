a2c41c2d7273283acdd2de47aab36437a6385c48
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-05 11:46:42 +0300

    220600 / Рефакторинг новостей и машин - Постраничная навигация

--- app/Contracts/ArticlesRepositoryContract.php
+++ app/Contracts/ArticlesRepositoryContract.php
@@ -5,6 +5,7 @@ namespace App\Contracts;
 interface ArticlesRepositoryContract
 {
     public function getLatestPublishedArticles($count);
+    public function getPaginatedArticles(int $page);
     public function getAllLatestPublishedArticles();
     public function deleteArticle($article);
 
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -25,7 +25,7 @@ class ArticleController extends Controller
     public function index()
     {
         return view('pages.articles', [
-            'articles' => $this->articleRepository->getAllLatestPublishedArticles()
+            'articles' => $this->articleRepository->getPaginatedArticles(5)
         ]);
     }
 
