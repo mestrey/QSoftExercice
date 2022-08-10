9202cbd6d1fd376f52762e884192c6c8abcc13b4
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-10 15:54:44 +0300

    220602 / Изображения на сайте - Model Image

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'path' => $this->faker->image('public/pictures', 640, 480),
+            'alt' => $this->faker->word()
+        ];
+    }
+}
