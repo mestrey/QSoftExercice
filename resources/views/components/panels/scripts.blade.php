327f430060aab6eed0bfaac035bb9eda4761cf14
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-12 13:20:06 +0300

    220607 / Работа с Frontend - webpack

--- resources/views/components/panels/scripts.blade.php
+++ resources/views/components/panels/scripts.blade.php
@@ -3,4 +3,4 @@
 <link href="{{ asset('js/vendor/slick.css') }}" rel="stylesheet">
 <script src="{{ asset('js/vendor/slick.min.js') }}"></script>
 
-<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
