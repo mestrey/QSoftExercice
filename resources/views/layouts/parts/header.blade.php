e4eaba233f0d87259933d3897efd9c7ad2990361
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-12 10:57:33 +0300

    220606 / Auth - Контроллерс и header

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -7,9 +7,33 @@
                 </a>
             </div>
             <div>
+                @auth
                 <ul class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
                     <li>
-                        <a class="text-gray-500 hover:text-orange" href="authorized.html">
+                        <a class="text-gray-500 hover:text-orange" href="{{ route('account') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            </svg>
+                            {{ Auth::user()->name }}
+                        </a>
+                    </li>
+                    <li>
+                        <a class="text-gray-500 hover:text-orange" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                            </svg>
+                            Выйти
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+                @endauth
+                @guest
+                <ul class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
+                    <li>
+                        <a class="text-gray-500 hover:text-orange" href="{{ route('register') }}">
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                             </svg>
@@ -17,7 +41,7 @@
                         </a>
                     </li>
                     <li>
-                        <a class="text-gray-500 hover:text-orange" href="form.html">
+                        <a class="text-gray-500 hover:text-orange" href="{{ route('login') }}">
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                             </svg>
@@ -25,6 +49,7 @@
                         </a>
                     </li>
                 </ul>
+                @endguest
             </div>
         </div>
     </div>
