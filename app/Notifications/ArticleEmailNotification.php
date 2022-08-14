fccdb11232a00dd21dc32e23109ea8cd9833a2d6
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-14 10:38:14 +0300

    220609 / Отправка уведомления - Отправка уведомления о событии

---
+++ app/Notifications/ArticleEmailNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ArticleEmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected array $data
+    ) {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting($this->data['title'] ?? 'Title')
+            ->line($this->data['body'] ?? 'Body')
+            ->action(
+                $this->data['name'] ?? 'Article',
+                url($this->data['url'] ?? '/')
+            );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
