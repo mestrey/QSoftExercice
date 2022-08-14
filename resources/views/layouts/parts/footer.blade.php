0a4011a1979f6a379503be0e5cf43d430aefd17e
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-14 09:50:52 +0300

    220606 / Цепочка навигации

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -6,7 +6,7 @@
         <div class="mt-8 border-t sm:border-t-0 sm:mt-0 sm:border-l py-2 sm:pl-4 sm:pr-8">
             <p class="text-3xl text-black font-bold mb-4">Информация</p>
             <nav>
-                <ul class="list-inside  bullet-list-item">
+                <ul class="list-inside bullet-list-item">
                     <li><a class="@if (request()->is('about')) text-orange cursor-default @else text-gray-600 hover:text-orange @endif" href="/about">О компании</a></li>
                     <li><a class="@if (request()->is('contact')) text-orange cursor-default @else text-gray-600 hover:text-orange @endif" href="/contact">Контактная информация</a></li>
                     <li><a class="@if (request()->is('condition')) text-orange cursor-default @else text-gray-600 hover:text-orange @endif" href="/condition">Условия продаж</a></li>
