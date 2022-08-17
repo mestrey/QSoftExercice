244fe40206fd9232d6d7529c2f94ad3d757ddec5
Местре Йован <mestre_i_qschool@qsoft.ru>
2022-08-17 12:16:48 +0300

    220612 / Создание api Cars - Самопроверка задачи

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('cars', CarApiController::class, [
-    'except' => ['show', 'edit', 'create'],
+Route::apiResource('cars', CarApiController::class, [
     'middleware' => 'shield'
 ]);
